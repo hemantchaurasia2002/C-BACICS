6a269da847644d7c92df0b78f43e9a84d3762337
Hemant Chaurasia <62471000+hemantchaurasia2002@users.noreply.github.com>
2020-10-31 20:05:33 +0530

    2ND COMMIT

---
+++ strcpy.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char s1[20]="Hemant";
+    char s2[10]="";
+
+    strcpy(s2,s1);
+    cout<<s2<<endl;
+
+}   
+//strcopy function is used to copy one string to another
---
+++ strstr.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char s1="Programming";
+    char s2="g";
+
+    cout<<strstr(s1,s2)<<endl;
+}//strstr is used to finding occurrance of any character in string 
