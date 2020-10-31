19eea9d2b87043393fea42b31630cfa5946f93ca
Hemant Chaurasia <62471000+hemantchaurasia2002@users.noreply.github.com>
2020-10-31 19:30:14 +0530

    1ST COMMIT

---
+++ strcat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char s1[20]="Good";
+    char s2[10]="Morning";
+    strcat(s1,s2);
+    cout<<s1<<endl;
+    
+}
+// in this program i used strcat function of string which is mainly used for merging two strings//
